932e91b1a0b539d4863d535a9ea1ba286982ae80
Mobilewits Mobile apps developers <harry@Harrys-Mac-Pro.local>
2015-03-25 13:35:48 -0400

    How to Pass Data in Pages using Xamarin Forms

---
+++ PassDataXamarinForms/MainPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace PassDataXamarinForms
+{
+	public class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+
+			Entry txt_name = new Entry {
+				WidthRequest=250,
+				HorizontalOptions = LayoutOptions.Start,
+				Placeholder="Enter Name"
+			};
+			var txtlayout = new StackLayout {
+				Padding= new Thickness(35,20,0,0),
+				Children={
+					txt_name,
+				},
+					
+			};
+			txtlayout.WidthRequest = 10;
+			Button btn_send = new Button{
+				Text="Send"
+			};
+
+			btn_send.Clicked += (sender, e) => {
+				Navigation.PushAsync(new SecondPage(txt_name.Text));
+			};
+
+
+			Content = new StackLayout { 
+				Children = {
+					txtlayout,btn_send
+				}
+			};
+			Title="First Page";
+		}
+	}
+}
+
+
---
+++ PassDataXamarinForms/SecondPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace PassDataXamarinForms
+{
+	public class SecondPage : ContentPage
+	{
+		public SecondPage (string data)
+		{
+			string Name = data;
+			Label lbl_name = new Label {
+				Text ="hello "+ Name
+			};
+			Content = new StackLayout { 
+				Children = {
+					lbl_name
+				}
+			};
+			Title="Second Page";
+		}
+	}
+}
+
+
